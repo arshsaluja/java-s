1baf8f97fb693e420482efddb53e5e2308489e47
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-01-27 17:24:49 +0530

    Create P3.java
    *
    **
    ***
    ****
    ****
    ***
    **
    *

---
+++ Pattern/P3.java
@@ -0,0 +1,35 @@
+/* *
+**
+***
+****
+****
+***
+**
+*  */
+import java.util.*;
+class P1
+{
+public static void main(String args[])
+{
+Scanner sc =new Scanner (System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+
+for(int i=1;i<=n;i++)
+{
+for(int j=4;j>=i;j--)
+{
+System.out.print("*");
+}
+System.out.println(" ");
+}
+}
+}
+
