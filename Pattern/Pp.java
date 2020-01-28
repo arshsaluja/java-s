446f4355426026addd61d263ea63dbbbf288cc61
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-01-28 12:54:21 +0530

    Create Pp.java

---
+++ Pattern/Pp.java
@@ -0,0 +1,24 @@
+/*  _ _ _ *
+    _ _ *  
+    _ * 
+*/
+class Pp
+{
+public static void main(String args[])
+{
+for(int i=1;i<4;i++)
+{
+for(int k=3;k>=i;k--)
+{
+System.out.print(" ");
+}
+for(int j=1;j<=1;j++)
+{
+System.out.print("*");
+}
+
+System.out.println("");
+
+}
+}
+}
