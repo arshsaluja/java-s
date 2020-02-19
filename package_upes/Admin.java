1638b9d79f84dc54e6b757984fbeaca07b0980c1
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-19 12:31:02 +0530

    package-upes

---
+++ package_upes/Admin.java
@@ -0,0 +1,15 @@
+package adminupes;
+import upes.Ifm1;
+//import upes.*;
+ class Admin
+{
+public static void main(String args[])
+{
+Ifm1 i = new Ifm1();
+i.run();
+}
+}
+//to compie and execute
+//javac -d . Ifm1.java
+//javac -d .Admin.java
+//java adminupes.Admin
